1a7b525e2c2cfa599efe78acafb4774488dce4e8
logictkt <logictkt@gmail.com>
2018-06-27 18:40:28 +0900

    上書きに変更

--- main.rb
+++ main.rb
@@ -5,8 +5,7 @@ Dotenv.load
 Tinify.key = ENV['TINIFY_KEY']
 
 ARGV.each do |path|
+  p "convertint #{path}..."
   source = Tinify.from_file(path)
-  dir_name = File.dirname(path)
-  origin_filename = File.basename(path)
-  source.to_file(File.join(dir_name, "#{origin_filename}.optimized"))
+  source.to_file(path)
 end
